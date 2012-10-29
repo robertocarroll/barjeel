aabc0c9aa6300f50d2c4e9c63d5a45d41144bc6e
Robert Carroll <robertocarroll@gmail.com>
2012-10-29 11:04:21 +0000

    Updated HTML5 shiv to latest minified version.

--- header.php
+++ header.php
@@ -15,7 +15,15 @@
 
 <!-- Meta Data & SEO -->
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+
+  <!--  Mobile Viewport Fix
+        j.mp/mobileviewport & davidbcalhoun.com/2010/viewport-metatag 
+  device-width : Occupy full width of the screen in its current orientation
+  initial-scale = 1.0 retains dimensions instead of zooming out if page height > device height
+  maximum-scale = 1.0 retains dimensions instead of zooming in if page width < device width
+  -->
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+
 
 <title><?php wp_title(''); ?></title>
 

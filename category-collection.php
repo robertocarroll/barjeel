fd4c5db75e93b7d36cc7b880e14a9891992f923d
Robert Carroll <robertocarroll@gmail.com>
2012-11-21 15:40:04 +0000

    Added character limit to collection titles.

--- category-collection.php
+++ category-collection.php
@@ -41,7 +41,12 @@
 								
 								</div><!-- .center -->	
 							
-								<h3 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+								<h3 class="title"><a href="<?php the_permalink(); ?>">
+
+									<?php $title = get_the_title(); 
+									echo mb_strimwidth($title, 0, 20, '...'); 
+									?>
+								</a></h3>
 								
 								<p class="artist"><?php echo rw_get_the_term_list(null, 'artist', false, '', ', ', ''); ?></p>
 									

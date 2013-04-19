6a1d468c87ab5a6c9a7e4ba8cb11eb138e826af1
robertocarroll <robertocarroll@gmail.com>
2013-04-19 15:52:32 +0100

    Added see more to the news category menu and a page for all the news categories. Fixes #8

---
+++ page-news-categories.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Template Name: News category list
+
+http://wpquestions.com/question/show/id/3864
+
+http://wordpress.stackexchange.com/questions/24794/get-the-the-top-level-parent-of-a-custom-taxonomy-term
+
+*/
+?>
+
+<?php get_header(); ?>
+
+
+<div class="pad-in white">
+
+	<div class="entry-content">
+
+<?php 
+//list terms in a given taxonomy using wp_list_categories (also useful as a widget if using a PHP Code plugin)
+
+$taxonomy     = 'news-themes';
+$orderby      = 'name'; 
+$show_count   = 0;      // 1 for yes, 0 for no
+$pad_counts   = 0;      // 1 for yes, 0 for no
+$hierarchical = 0;      // 1 for yes, 0 for no
+$title        = '';
+
+$args = array(
+  'taxonomy'     => $taxonomy,
+  'orderby'      => $orderby,
+  'show_count'   => $show_count,
+  'pad_counts'   => $pad_counts,
+  'hierarchical' => $hierarchical,
+  'hide_empty' => 1,
+  'title_li'     => $title
+);
+?>
+
+<ul class="country-list">
+	<?php wp_list_categories( $args ); ?>
+</ul>
+
+</div><!-- .entry-content -->
+
+</div><!-- .pad-in -->
+
+<?php get_footer(); ?>

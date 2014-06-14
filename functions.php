c6eb749d0837698355d50cab7981691c30094aa9
robertocarroll <robertocarroll@gmail.com>
2014-06-14 11:16:55 +0100

    Fixed the pagination to make it work better on smaller devices.

--- functions.php
+++ functions.php
@@ -539,9 +539,9 @@ function base_pagination() {
 		'base' => str_replace( $big, '%#%', get_pagenum_link($big) ),
 		'current' => max( 1, get_query_var('paged') ),
 		'total' => $wp_query->max_num_pages,
-		'prev_text'    => __('Previous'),
-		'next_text'    => __('Next'),
-		'end_size' => 4,
+		'prev_text'    => __('«'),
+		'next_text'    => __('»'),
+		'end_size' => 1,
 		'mid_size' => 2
 	) );
  

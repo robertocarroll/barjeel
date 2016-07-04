738c3d871134a79865c75c0d6f1928df1b6e28aa
robertocarroll <rob@wearethoughtfox.com>
2016-07-04 14:56:02 +0100

    Added filter to show all artworks on exhibition layout.

--- content-aside.php
+++ content-aside.php
@@ -2,7 +2,7 @@
 /**
  * @package barjeel
  * @since barjeel 1.0
- * Template for the exhibitions posts 
+ * Template for the exhibitions posts
  */
 ?>
 
@@ -14,7 +14,7 @@
 				$royal_slideshow = get_post_meta($post->ID, 'slideshow', true);
 				if ($royal_slideshow)
 				{
-				
+
 			?>
 
 				<?php echo '<div class="exhibition-slideshow">' ?>
@@ -24,39 +24,39 @@
 				<?php echo '</div>' ?>
 
 				<?php } ?>
-						
-		<div class="exhibition-text white">						
+
+		<div class="exhibition-text white">
 
 					<h1 class="alpha bold exhibition-title gray"><?php the_title(); ?></h1>
 
-								<?php $dates = get_post_meta($post->ID, 'Dates', true); ?>	
+								<?php $dates = get_post_meta($post->ID, 'Dates', true); ?>
 
-								<?php $location = get_post_meta($post->ID, 'Location', true); ?>	
+								<?php $location = get_post_meta($post->ID, 'Location', true); ?>
 
 								<?php $exhibition_meta = array ($dates, $location) ?>
 
 								<?php	if(!empty($exhibition_meta)) { ?>
 
-									<?php echo '<ul class="exhibition-meta-list">' ?>	
+									<?php echo '<ul class="exhibition-meta-list">' ?>
 
-									<?php	
+									<?php
 
 										foreach($exhibition_meta as $value) {
 			  							print '<li>'.$value.'</li>'; ;
 										}
-									?>		
-				
-									<?php echo '</ul>' ?>	
+									?>
+
+									<?php echo '</ul>' ?>
 									<?php } ?>
 
-	
+
 					<div class="exhibition-main-text">
 
 						<?php the_content(); ?>
-					
-					</div><!-- .exhibition-main-text -->	
 
-			</div><!-- .white -->	
+					</div><!-- .exhibition-main-text -->
+
+			</div><!-- .white -->
 
 		</div><!-- .exhibition-main -->
 
@@ -65,49 +65,49 @@
 
 	<!-- Related posts - manually chosen posts  -->
 
-			
+
 			<?php
 			    $barjeel_style_classes = array('article-one','article-two','article-three', 'article-four');
 			    $barjeel_styles_count = count($barjeel_style_classes);
 			    $barjeel_style_index = 0;
 			?>
-			
+
 			<?php
-				
+
 				if ( !function_exists( 'p2p_register_connection_type' ) )
-				
+
 				return;
-	
+
 				global $wp_query;
-				
-				p2p_type( 'related-articles' )->each_connected( $wp_query );	
+
+				p2p_type( 'related-articles' )->each_connected( $wp_query );
 
 					global $post;
 					if( isset( $post->connected ) && !empty( $post->connected ) ):
 
-		echo '<div class="related-posts">'; 			
-						
+		echo '<div class="related-posts">';
+
 						$count = 1;
-						
+
 						foreach( $post->connected as $related ):
-							
+
 							if( $count < 4 ) {	?>
 
-				<article>	
+				<article>
 
-					<div class="related-list <?php echo $barjeel_style_classes[$barjeel_style_index++ % $barjeel_styles_count]; ?>">	
+					<div class="related-list <?php echo $barjeel_style_classes[$barjeel_style_index++ % $barjeel_styles_count]; ?>">
 
 							<div class="square">&nbsp;</div>
 
 							<?php echo '<a href="' . get_permalink( $related->ID ) . '">'; ?>
 
 									<div class="vignette-square">
-			 
-										 <?php if (class_exists('MultiPostThumbnails')) : MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'feature-image-2', $related->ID,  'cropped-thumb'); 
 
-										 endif; ?>		
+										 <?php if (class_exists('MultiPostThumbnails')) : MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'feature-image-2', $related->ID,  'cropped-thumb');
 
-									</div>	
+										 endif; ?>
+
+									</div>
 
 								</a>
 
@@ -118,31 +118,39 @@
 								if ($dates) { ?>
 								<?php echo '<h2 class="date epsilon e-date">'.$dates.'</h2>'; ?>
 							<?php } ?>
-										
-					</div>	
+
+					</div>
 
 				</article>
-								
 
-						<?php		
+
+						<?php
 								$count++;
-							
+
 
 							}
 						endforeach;
 
-		echo '</div>';	
-					
+		echo '</div>';
+
 					endif;
 
 
-?>	
+?>
+
+<?php /* Reset query */ wp_reset_query();
+
+  /* Setting a crazy limit here */
+  add_filter('post_limits', 'your_query_limit');
+  function your_query_limit($limit){
+      return "LIMIT 500";
+}
+?>
 
 <!-- Artwork - looks for posts with a category collection and a taxonomy exhibition -->
-		
+
 		<?php global $post;
 			$artwork['tax_query'] = array(
-				'nopaging' => 'true',
 				array(
 					'taxonomy' => 'category',
 					'terms' => array('collection'),
@@ -153,34 +161,34 @@
 					'terms' => array($post->post_name),
 					'field' => 'slug',
 				),
+        'posts_per_page' => -1
 			);
+
 			?>
 
 			<div class="related-artwork">
-			
-				<?php $artwork_query = new WP_Query( $artwork ); ?>
-				
-				<?php /* Start the Loop */ ?>
-				
-				<?php if( $artwork_query->have_posts() ) : ?>
-				
-				<h2 class="related-title">Artwork from <?php the_title(); ?> </h2> 
-		
-				<div id="sort">
-				
-				<?php while ( $artwork_query->have_posts() ) : $artwork_query->the_post(); ?>
-		
-					<?php get_template_part('catalogue'); ?>
-				
-				<?php endwhile; ?>
-				
-				</div>
-				
-				<?php endif; ?>	
-				
-				<?php /* Reset query */ wp_reset_postdata(); ?>
 
-			</div>	
+				<?php $artwork_query = new WP_Query( $artwork ); ?>
+
+				<h2 class="related-title">Artwork from <?php the_title(); ?> </h2>
+
+				<div id="sort">
+
+				<?php while ( $artwork_query->have_posts() ) : $artwork_query->the_post(); ?>
+
+					<?php get_template_part('catalogue'); ?>
+
+				<?php endwhile; ?>
+
+				</div>
+
+				<?php /* Reset query */ wp_reset_query();
+
+        remove_filter('post_limits', 'your_query_limit');
+
+        ?>
+
+			</div>
 
 
 

15a476f0578512a08e32ad6c92f39050c0297fd4
robertocarroll <rob@wearethoughtfox.com>
2016-10-21 20:08:43 +0400

    Removed the_post_thumbnail_caption because it sent 4.6 crazy. Doesn't seem to be being used anywhere in the theme.

--- functions.php
+++ functions.php
@@ -393,38 +393,7 @@ endif; // barjeel_content_nav
 |
 | */
 
-function the_post_thumbnail_caption() {
-  global $post;
 
-  $thumb_id = get_post_thumbnail_id($post->id);
-
-  $args = array(
-	'post_type' => 'attachment',
-	'post_status' => null,
-	'post_parent' => $post->ID,
-	'include'  => $thumb_id
-	);
-
-   $thumbnail_image = get_posts($args);
-
-   if ($thumbnail_image && isset($thumbnail_image[0])) {
-     //show thumbnail title
-     //echo $thumbnail_image[0]->post_title;
-
-     //Uncomment to show the thumbnail caption
-     echo '<div class="f_caption"><p>'.$thumbnail_image[0]->post_excerpt.'</p></div>';
-
-     //Uncomment to show the thumbnail description
-     //echo $thumbnail_image[0]->post_content;
-
-     //Uncomment to show the thumbnail alt field
-     //$alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
-     //if(count($alt)) echo $alt;
-  }
-
-  else {echo '<div class="bottom"></div>';}
-
-}
 
 /*
 | -------------------------------------------------------------------

a7736624f0d59a582d388d1433df68923e1c3bde
robertocarroll <rob@wearethoughtfox.com>
2017-05-02 12:08:43 +0400

    Added the flex to the id for the related artworks.

--- content.php
+++ content.php
@@ -6,39 +6,21 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
+	<div class="news-item">
+    <!-- Gets the cropped image -->
+    <div class="news-image">
+      <a href="<?php the_permalink(); ?>">
+        <?php if (class_exists('MultiPostThumbnails')) : MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'feature-image-2', NULL,  'news-cropped-thumb');
+          endif; ?>
+        </a>
+    </div>
 
-		<div class="news-item">	
-
-			<!-- Gets the cropped image -->
-
-			<div class="news-image">	
-
-				<a href="<?php the_permalink(); ?>">	
-
-				 <?php if (class_exists('MultiPostThumbnails')) : MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'feature-image-2', NULL,  'news-cropped-thumb'); 
-
-				 endif; ?>			
-
-				</a>	
-
-			</div>	
-
-			
-			<div class="news-text pad-in">
-
-					<h1 class="search-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>	
-
-					<span class="epsilon light small gray margin-below permalink"><?php the_permalink(); ?></span> 	
-
-					<h2 class="epsilon date margin-below-half padding-top"><?php the_time('j F Y'); ?></h3>
-
-					<?php echo rw_get_the_term_list(null, 'artist', false, '', ', ', ''); ?>	
-					
-					<?php if (function_exists('relevanssi_the_excerpt')) { relevanssi_the_excerpt(); }; ?>
-
-			</div>							
-
-			</div>
-
+  <div class="news-text pad-in">
+    <h1 class="search-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+      <span class="epsilon light small gray margin-below permalink"><?php the_permalink(); ?></span>
+        <h2 class="epsilon date margin-below-half padding-top"><?php the_time('j F Y'); ?></h3>
+        <?php echo rw_get_the_term_list(null, 'artist', false, '', ', ', ''); ?>
+        <?php if (function_exists('relevanssi_the_excerpt')) { relevanssi_the_excerpt(); }; ?>
+      </div>
+  </div>
 </article><!-- #post-<?php the_ID(); ?> -->

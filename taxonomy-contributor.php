982f69705f25bbc47dd3670d0c14acdd391fcda2
robertocarroll <rob@wearethoughtfox.com>
2017-09-30 18:34:19 +0400

    Logic for the contributors layout.

--- taxonomy-contributor.php
+++ taxonomy-contributor.php
@@ -5,6 +5,7 @@
     <?php
     $queried_object = get_queried_object();
     $term_id = $queried_object->term_id;
+    $taxonomy = $queried_object->taxonomy;
     $image_key = 'portrait';
     $firstname_key = 'first-name';
     $lastname_key = 'last-name';
@@ -27,6 +28,109 @@
     print_r($lastname);
     print_r($email);
     print_r($summary);
+
+    ?>
+
+    <?php $exhibition['tax_query'] = array(
+
+        array(
+          'taxonomy' => 'category',
+          'terms' => array('exhibitions'),
+          'field' => 'slug',
+        ),
+
+        array(
+          'taxonomy' => $taxonomy,
+          'terms' => array($queried_object->slug),
+          'field' => 'slug',
+        )
+      );
+
+      ?>
+
+    <?php $exhibition_query = new WP_Query( $exhibition );
+          if($exhibition_query->have_posts()) {
+           ?>
+
+    <p>Curating by <?php print_r($firstname); ?></p>
+
+    <?php while ( $exhibition_query->have_posts() ) : $exhibition_query->the_post(); ?>
+
+    <?php $title = get_the_title();
+          print_r($title);
+    ?>
+
+    <?php endwhile;
+        }
+     ?>
+
+    <?php /* Reset query */ wp_reset_query();
+           remove_filter('post_limits', 'your_query_limit');
+          ?>
+
+    <?php $news['tax_query'] = array(
+
+        array(
+          'taxonomy' => 'category',
+          'terms' => array('news'),
+          'field' => 'slug',
+        ),
+
+        array(
+          'taxonomy' => $taxonomy,
+          'terms' => array($queried_object->slug),
+          'field' => 'slug',
+        )
+      );
+
+      ?>
+
+    <?php $news_query = new WP_Query( $news );
+           if($news_query->have_posts()) {
+    ?>
+
+    <p>Writing by <?php print_r($firstname); ?></p>
+
+    <?php while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
+
+   <p> <?php $title = get_the_title();
+          print_r($title);
+    ?></p>
+
+    <?php endwhile;
+          }
+     ?>
+
+    <?php /* Reset query */ wp_reset_query();
+           remove_filter('post_limits', 'your_query_limit');
+          ?>
+
+    <?php
+
+    // get the other contributors and exclude the current one
+
+    $taxonomies = array(
+      $taxonomy
+    );
+
+    $args = array(
+    'orderby'       => 'name',
+    'order'         => 'ASC',
+    'hide_empty'    => false,
+    'exclude'       => array($term_id)
+    );
+
+
+    $all_contributors = get_terms( $taxonomies , $args);
+
+     foreach ( $all_contributors as $contributor ) {
+        $contributor_term_id = $contributor->term_id;
+        $contributor_firstname = wp_get_terms_meta($contributor_term_id, $firstname_key ,true);
+        $contributor_image = wp_get_terms_meta($contributor_term_id, $image_key ,true);
+        print_r($contributor_image);
+        print_r($contributor_firstname);
+
+    }
 
     ?>
 

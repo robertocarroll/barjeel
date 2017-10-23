1d439287874a41e96b5e080cd7d4f93081aafac3
robertocarroll <rob@wearethoughtfox.com>
2017-10-23 15:24:05 +0400

    Added written by to news layout.

--- news.php
+++ news.php
@@ -1,36 +1,18 @@
 <article>
-
-			<div class="news-item">	
-
-			<!-- Gets the cropped image -->
-
-			<div class="news-image">	
-
-				<a href="<?php the_permalink(); ?>">	
-
-				 <?php if (class_exists('MultiPostThumbnails')) : MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'feature-image-2', NULL,  'news-cropped-thumb'); 
-
-				 endif; ?>			
-
-				</a>	
-
-			</div>	
-
-			
-			<div class="news-text pad-in">
-
-					<h2 class="zeta date margin-below-half gray"><?php the_time('j F Y'); ?></h3>
-
-					<h1 class="gamma bold big-line margin-below-half"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>		
-					
-					<ul class="entry-meta-news light uppercase meta-news nav">		
-
-				<?php echo get_the_term_list( get_the_ID(), 'news-themes', '<li class="meta-link">', '</li><li class="meta-link">', '</li>') ?>	
-
-					</ul>	
-
-			</div>							
-
-			</div>
-
-		</article>
+  <div class="news-item">
+  <!-- Gets the cropped image -->
+    <div class="news-image">
+      <a href="<?php the_permalink(); ?>">
+        <?php if (class_exists('MultiPostThumbnails')) : MultiPostThumbnails::the_post_thumbnail(get_post_type(), 'feature-image-2', NULL,  'news-cropped-thumb');
+        endif; ?>
+      </a>
+    </div>
+    <div class="news-text pad-in">
+      <h2 class="zeta date margin-below-half gray"><?php the_time('j F Y'); ?></h3>
+        <h1 class="gamma bold big-line margin-below-half"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+        <ul class="entry-meta-news light uppercase meta-news nav">
+          <?php echo get_the_term_list( get_the_ID(), 'news-themes', '<li class="meta-link">', '</li><li class="meta-link">', '</li>') ?>
+        </ul>
+    </div>
+  </div>
+</article>

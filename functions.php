64ad59e9a13acefcbce47af4c1bfe997af5d27eb
robertocarroll <rob@wearethoughtfox.com>
2017-11-11 23:41:23 +0400

    Added remove empty paragraphs filter.

--- functions.php
+++ functions.php
@@ -850,3 +850,15 @@ foreach ( array( 'term_description' ) as $filter ) {
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
 
+/**
+ * Remove empty paragraphs created by wpautop()
+ * @author Ryan Hamilton
+ * @link https://gist.github.com/Fantikerz/5557617
+ */
+function remove_empty_p( $content ) {
+    $content = force_balance_tags( $content );
+    $content = preg_replace( '#<p>\s*+(<br\s*/*>)?\s*</p>#i', '', $content );
+    $content = preg_replace( '~\s?<p>(\s|&nbsp;)+</p>\s?~', '', $content );
+    return $content;
+}
+add_filter('the_content', 'remove_empty_p', 20, 1);
